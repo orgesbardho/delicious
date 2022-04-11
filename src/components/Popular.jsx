1d9374df3cb9cfe78cc0c8e8f43264ab721fc7f3
orgesbardho <orgesbardho93@gmail.com>
2022-04-11 21:08:16 +0200

    min 26

--- src/components/Popular.jsx
+++ src/components/Popular.jsx
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function Popular() {
-
+  
+  const [popular, setPopular] = useState([])
   useEffect(()=>{
     getPopular();
   },[]);
@@ -11,12 +12,22 @@ function Popular() {
     https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
     );
     const data = await api.json();
-    console.log(data);
+    setPopular(data.recipes)
   }
 
 
   return (
-    <div>Popular</div>
+    <div>
+
+      {popular.map((recipe ) => {
+        return(
+          <div key={recipe.id}>
+            <p>{recipe.title}</p>
+          </div>
+        );
+      })}
+    </div>
+
   )
 }
 
