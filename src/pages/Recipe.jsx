a1c8997fed753c9009b5ab7136323d2fde110f08
orgesbardho <orgesbardho93@gmail.com>
2022-04-13 21:04:44 +0200

    fixed!

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -35,6 +35,11 @@ function Recipe() {
           <h3 dangerouslySetInnerHTML={{__html: details.summary}}></h3>
           <h3 dangerouslySetInnerHTML={{__html: details.instructions}}></h3>
         </div>
+        <ul>
+          {details.extendedIngredients.map((ingredient) => 
+          <li key={ingredient.id}>{ingredient.original}</li>
+          )}
+        </ul>
       </Info>
     </DetailWrapper>
   )
